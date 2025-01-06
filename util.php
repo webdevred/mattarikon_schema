a58aff97b9671696368026811e2c74da939b9efd
mangakai.org <mangakai.org@mangakai.org>
2025-01-06 14:19:01 +0000

    upgrade urls to more human readable style

--- util.php
+++ util.php
@@ -2,6 +2,20 @@
 
 require("config.php");
 
+if (file_exists(__DIR__ ."/.htaccess") and $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $request = $_SERVER['REQUEST_URI'];
+
+    if (str_ends_with($_SERVER["REQUEST_URI"], "index.php")) {
+        $newUrl = dirname($request);
+        header("Location: $newUrl", true, 301);
+	exit;
+    } elseif (strpos($request, '.php') !== false) {
+        $newUrl = str_replace('.php', '', $request);
+        header("Location: $newUrl", true, 301);
+	exit;
+    }
+}
+
 // Create connection
 $conn = new mysqli(SERVER_HOSTNAME, USERNAME, PASSWORD, DATABASE_NAME);
 // Check connection
