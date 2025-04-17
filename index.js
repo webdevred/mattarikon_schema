63008a1ebd1904987a74af1e6b96a90cf04e9b69
mangakai.org <148627186+webdevred@users.noreply.github.com>
2025-04-17 11:28:09 +0000

    add whole day activities

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-    setTimeout(function() { window.location.reload(); }, 60000);
-    
-    let urlParams = new URL(window.location).searchParams;
-    let beingDisplayedOnMonitor = urlParams.get("m") == "1";
-    if(beingDisplayedOnMonitor) {
+setTimeout(function() { window.location.reload(); }, 60000);
+
+let urlParams = new URL(window.location).searchParams;
+let beingDisplayedOnMonitor = urlParams.get("m") == "1";
+if(beingDisplayedOnMonitor) {
     refresh_handler = function(e) {
         for(let element of document.querySelectorAll("section.activity")) {
             var boundingClientRect = element.getBoundingClientRect();
-            if ( window.innerHeight < boundingClientRect.bottom ) {
-                console.log(boundingClientRect);
+
+            const top = boundingClientRect.top;
+            const bottom = boundingClientRect.bottom;
+            const elementHeight = boundingClientRect.height;
+            const windowHeight = window.innerHeight;
+            if(windowHeight > top + elementHeight / 3 && windowHeight < bottom) {
+                const overlap = bottom - windowHeight;
+                const newHeight = elementHeight - overlap;
+                let description = element.querySelector("p");
+                description.style.maxHeight = newHeight / 3;
+                description.classList.add("break");
+            } else if( windowHeight < bottom ) {
                 element.style.display = "none";
             }
         }
-   };
+    };
 
-   window.addEventListener('scroll', refresh_handler);
-   window.addEventListener('load', refresh_handler);
-   window.addEventListener('resize', refresh_handler);
+    window.addEventListener('scroll', refresh_handler);
+    window.addEventListener('load', refresh_handler);
+    window.addEventListener('resize', refresh_handler);
 }
--- util.php
+++ util.php
@@ -1,13 +1,15 @@
 <?php session_start(); 
 
 require("config.php");
-
+$minute = "";
 $datetime = new DateTime(null, new DateTimeZone("Europe/Stockholm"));
 if( ! isset($_GET["t"])) {
     $current_time = $datetime->format("H:i");
 } else {
     $current_time = $_GET["t"];
 }
+
+$minute = substr($current_time, 3, 2);
 
 if (file_exists(__DIR__ ."/.htaccess") and $_SERVER['REQUEST_METHOD'] === 'GET') {
     $request = $_SERVER['REQUEST_URI'];
@@ -36,7 +38,17 @@ function list_activities($filter = 0) {
         $qwhere = "WHERE ut.start_time > CAST('" . $current_time . "' AS TIME)";
 	break;
     }
-    $activities = $conn->query("WITH time_and_place_ids AS ( SELECT DISTINCT activity_id,
+
+    $monitor_column = " 'ACTIVITY' ";
+    if($filter != 0 and isset($_GET["m"])) {
+        if ($filter == 3) {
+            $monitor_column = " IF(TIMEDIFF(ut.end_time,ut.start_time) > CAST('03:00:00' AS TIME), 'FULLDAY', 'ACTIVITY') ";
+            $qwhere = "WHERE TIMEDIFF(ut.end_time,ut.start_time) > CAST('03:00:00' AS TIME) ";
+        } else {
+            $qwhere .= "AND TIMEDIFF(ut.end_time,ut.start_time) <= CAST('03:00:00' AS TIME) ";
+        }
+    }
+    $sql = "WITH time_and_place_ids AS ( SELECT DISTINCT activity_id,
             min(id) OVER(PARTITION by activity_id ) AS outdated_time_id,
             max(id) OVER(PARTITION by activity_id) AS updated_time_id
         FROM
@@ -49,14 +61,16 @@ function list_activities($filter = 0) {
               TIME_FORMAT(ot.start_time, '%H:%i') AS outdated_start_time,
               TIME_FORMAT(ot.end_time, '%H:%i') AS outdated_end_time,
               at.icon_filename, at.display_name AS type_name,
+              IF(type = 'MOVIE', 'MOVIE', " . $monitor_column . ") AS activity_column,
               ROW_NUMBER() OVER (PARTITION BY type = 'MOVIE' ORDER BY updated_start_time) as type_rownumber
               FROM activities AS a
               INNER JOIN activity_types AS at ON a.type = at.name
               INNER JOIN time_and_place_ids AS ti ON a.id = ti.activity_id 
               INNER JOIN activities_time_and_place AS ut ON ut.id = ti.updated_time_id
               LEFT JOIN activities_time_and_place AS ot ON ti.outdated_time_id <> ti.updated_time_id AND ot.id = ti.outdated_time_id
-              " . $qwhere ." ORDER BY updated_start_time");
+              " . $qwhere ." ORDER BY updated_start_time";
 
+    $activities = $conn->query($sql);
     return $activities;
 }
 
