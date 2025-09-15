5c63dbcebf9de7300fd659087d33bec7463c9a49
webdevred <148627186+webdevred@users.noreply.github.com>
2025-09-15 22:20:47 +0200

    Further improvements to new docker setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-cat config.template.php |
-  sed 's/${DATABASE_HOSTNAME}/'"$DATABASE_HOSTNAME"'/' |
-  sed 's/${DATABASE_USERNAME}/'"$DATABASE_USERNAME"'/' |
-  sed 's/${DATABASE_PASSWORD}/'"$DATABASE_PASSWORD"'/' |
-  sed 's/${DATABASE_NAME}/'"$DATABASE_NAME"'/' >config.php
+cat > ~/.my.cnf <<EOF
+[client]
+host=${DATABASE_HOSTNAME}
+user=${DATABASE_USERNAME}
+password=${DATABASE_PASSWORD}
+EOF
 
-mysql --skip-ssl-verify-server-cert -v -h "$DATABASE_HOSTNAME" \
-  -u "$DATABASE_USERNAME" \
-  -p"$DATABASE_PASSWORD" \
-  "$DATABASE_NAME" <./dump_schema.sql
+cnt=$(mysql --skip-ssl-verify-server-cert -sN -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$DATABASE_NAME';")
 
-mysql --skip-ssl-verify-server-cert -v -h "$DATABASE_HOSTNAME" \
-  -u "$DATABASE_USERNAME" \
-  -p"$DATABASE_PASSWORD" \
-  "$DATABASE_NAME" <./dump_baseline_data.sql
+echo "$DATABASE_NAME has $cnt tables"
+
+if ! [[ "${cnt}" =~ ^[0-9]+$ ]]; then
+  echo "Warning: table count is not a number: '${cnt}'" >&2
+fi
+
+if ! [[ "$cnt" -gt 0 ]]; then
+  sed -e "s|\${DATABASE_HOSTNAME}|${DATABASE_HOSTNAME}|g" \
+      -e "s|\${DATABASE_USERNAME}|${DATABASE_USERNAME}|g" \
+      -e "s|\${DATABASE_PASSWORD}|${DATABASE_PASSWORD}|g" \
+      -e "s|\${DATABASE_NAME}|${DATABASE_NAME}|g" \
+      config.template.php > config.php
+
+  echo "inserting data"
+
+  mysql --skip-ssl-verify-server-cert "$DATABASE_NAME" < ./dump_schema.sql
+
+  mysql --skip-ssl-verify-server-cert "$DATABASE_NAME" < ./dump_baseline_data.sql
+fi
 
 docker-php-entrypoint apache2-foreground
